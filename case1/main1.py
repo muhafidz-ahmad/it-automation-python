1b58550d0186f9f8d9bad00f9f301168c475892f
muhafidz-ahmad <muhafidz.ahmad@gmail.com>
2023-06-09 21:39:46 +0700

    change to create new spreadsheet

--- case1/main1.py
+++ case1/main1.py
@@ -7,14 +7,16 @@ def main():
     # --- Langkah-langkah untuk memakai program ini
     # Share file Google Sheet ke email Service Account di Google Cloud Project yang telah aktif Google Sheet API dan Google Drive API
     # Copy path keys Service Account pada variabel key_path di bawah
-    key_path = 'it-automation-dicoding-gsheet.json'
+    key_path = 'service-account.json'
     gc = auth(key_path)
 
-    # Buka file Google Sheets
-    sheet = gc.open('Kampus Merdeka Program')
+    # Buat file Google Sheets
+    sheet = gc.create('Program-Program Kampus Merdeka')
+    email_anda = 'your.email@example.com'
+    sheet.share(email_anda, perm_type='user', role='writer')
 
     # Buka Worksheet
-    worksheet = sheet.get_worksheet(0)
+    worksheet = sheet.worksheet("Sheet1")
 
     # Web Scraping
     df = get_data('https://kampusmerdeka.kemdikbud.go.id/program/studi-independen/browse/')
